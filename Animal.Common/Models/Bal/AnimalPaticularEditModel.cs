6d1708c244b91304a8c6685a9d810e301f7ab4f3
Dmytro.Rastsvietalov <Dmytro.Rastsvietalov@infopulse.com>
2017-12-15 22:57:19 +0200

    2

---
+++ Animal.Common/Models/Bal/AnimalPaticularEditModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Animal.Common.Models.Bal
+{
+	public class AnimalPaticularEditModel : AnimalParticularModel
+	{
+		public Dictionary<int,string> AnimalTypes { get; set; }
+
+		public AnimalPaticularEditModel()
+		{
+			AnimalTypes = new Dictionary<int, string>();
+		}
+	}
+}
---
+++ Animal.Web/Controllers/AnimalParticularController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Animal.Bal.Interfaces;
+using Animal.Common.Models.Bal;
+
+namespace Animal.Web.Controllers
+{
+	public class AnimalParticularController : Controller
+	{
+		private readonly IParticularAnimalService _animalParticularService;
+		private readonly IKindOfAnimalService _kindOfAnimalService;
+		public AnimalParticularController(IParticularAnimalService animalParticularService, IKindOfAnimalService kindOfAnimalService)
+		{
+			_animalParticularService = animalParticularService;
+			_kindOfAnimalService = kindOfAnimalService;
+		}
+
+
+/*
+		[HttpGet]
+		public ActionResult GetNewAnimalKind()
+		{
+			var animalKindEntity = new AnimalKindEntity();
+			return View("DifferentKindsOfAnimalsList", animalKindEntity);
+		}
+
+		[HttpPost]
+		public ActionResult AddNewAnimalKind(AnimalKindModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				_animalKindService.AddOrUpdateAnimalKind(model);
+				return View("DifferentKindsOfAnimalsList");
+			}
+			else
+			{
+				return View("AddingNewKindOfAnimal");
+			}
+		}
+*/
+
+
+		[HttpGet]
+		public ActionResult GetAnimalParticularAddingView()
+		{
+			var model = new AnimalPaticularEditModel
+			{
+				AnimalTypes = _kindOfAnimalService.GetAnimalKinds().ToDictionary(_ => _.Id, _ => _.Name)
+			};
+			return View("AddingNewParticularAnimal", model);
+		}
+
+		[HttpGet]
+		public ActionResult GetAnimalParticularView()
+		{
+			var model = new AnimalPaticularEditModel
+			{
+				AnimalTypes = _kindOfAnimalService.GetAnimalKinds().ToDictionary(_ => _.Id, _ => _.Name)
+			};
+			return View("DifferentAnimalsFromKindList", model);
+		}	
+
+		[HttpPost]
+		public ActionResult AddNewAnimalParticular(AnimalParticularModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				_animalParticularService.AddAnimalParticular(model);
+				return View("DifferentAnimalsFromKindList");
+			}
+			else
+			{
+				return View("AddingNewParticularAnimal");
+			}
+
+		}
+
+		[HttpGet]
+		public ActionResult GetByAnimalType(int id)
+		{
+			var animals = _animalParticularService.GetParticularAnimalStats();
+			return View("DifferentAnimalsFromKindList", animals);
+		}
+
+		[HttpPost]
+		public ActionResult Update()
+		{
+			return View("UpdateAnimalsParticular");
+		}
+
+		[HttpPost]
+		public ActionResult Delete()
+		{
+			return View("DifferentAnimalsFromKindList");
+		}
+
+
+		[HttpGet]
+		public ActionResult UpdateInformationAboutAnimal()
+		{
+			return View("UpdateAnimalsParticular");
+		}		
+	}
+}
