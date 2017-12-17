dc502599f7b07414b45b34413cea53b9c3f97037
Dmytro.Rastsvietalov <Dmytro.Rastsvietalov@infopulse.com>
2017-12-17 13:40:34 +0200

    .

---
+++ Animal.Web/Controllers/AccountApiController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Http;
+using Animal.Bal.Interfaces;
+using Animal.Common.Models.Bal;
+
+namespace Animal.Web.Controllers
+{
+
+	public class AccountApiController : ApiController
+	{
+		private readonly IAccountValidationService _accountValidationService;
+		private readonly IUserService _userService;
+
+		public AccountApiController(IAccountValidationService accountValidationService, IUserService userService)
+		{
+			_accountValidationService = accountValidationService;
+			_userService = userService;
+		}
+
+		public bool Register(UserModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var isEmailAlreadyExists = _accountValidationService.IsEmailAlreadyExists(model.Email);
+				if (isEmailAlreadyExists)
+				{
+					ModelState.AddModelError("Email", "Email already registered");
+				}
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool Login(LoginModel model)
+		{
+			return  _userService.LoginUser(model);			
+		}
+	}
+}
